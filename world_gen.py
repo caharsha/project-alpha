602fec00ce26ebec2e85d925000caf4919e952b9
caharsha <harshacopparam@gmail.com>
2014-12-11 21:31:05 -0600

    Added world_gen.py. Generating different worlds for testing agents.

---
+++ world_gen.py
@@ -0,0 +1,12 @@
+#
+# Harsha Ashokan Copparam
+# 
+# world_gen.py
+# generates wumpus worlds - given dimensions.
+# location of wumpus
+# location of pits
+# location of gold
+# dead ends
+# percepts - bump, scream, glitter, breeze, stench
+
+
